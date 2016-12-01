d2e5c5793ee5d12513ee054c5d958f22dcc04a5f
Jee Gikera <githinjigikera@gmail.com>
2016-12-01 16:04:40 +0300

    Add serializer docstrings

--- djangular/board/serializers.py
+++ djangular/board/serializers.py
@@ -1,17 +1,17 @@
-"""Serializers."""
+"""Serializers to encode python objects as json."""
 from rest_framework import serializers
 from .models import Card, List
 
 
 class ListSerializer(serializers.ModelSerializer):
-    """Serializer to change List fields to Json"""
+    """Autogenerate json based on the list fields."""
     class Meta:
         model = List
         fields = '__all__'
 
 
 class CardSerializer(serializers.ModelSerializer):
-    """Serializer to change Card fields to Json"""
+    """Autogenerate json based on the card fields"""
     class Meta:
         model = Card
         fields = '__all__'
